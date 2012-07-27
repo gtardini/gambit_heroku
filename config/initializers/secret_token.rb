0fc48a95659383067e8c17956655b4c244aebcf4
Giovanni Tardini <gtardini@gmail.com>
2012-07-27 20:17:33 +0200

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GambitHeroku::Application.config.secret_token = '891d14dc09bdc2562a34907d8ffc3075289c261fdf0e4fa136249a4c251ab03ad2453c5ecb38847bde6377085706f7620a08ad69691d02f72ecfa0ba9af1d7d2'
