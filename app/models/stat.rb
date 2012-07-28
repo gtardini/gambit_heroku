d945d9751277019b641e68dc669ed0b960475740
Giovanni Tardini <gtardini@gmail.com>
2012-07-28 20:41:35 +0200

    delayed job full test

--- app/models/stat.rb
+++ app/models/stat.rb
@@ -1,7 +1,4 @@
-require 'open-uri'
 class Stat < ActiveRecord::Base
 	def pushMail(oo, stats)
-		doc = Nokogiri::HTML(open("https://www.bwin.it/betViewIframe.aspx?SportID=4&bv=bb&selectedLeagues=0").read())
-		p doc
 	end
 end
